fadb9067c1112a2cfd456c67eb4a153c843fd5a6
gabrusso1903 <gabrielle.russo1@icloud.com>
2026-03-17 09:34:58 -0300

    lista de compras
    html e js

---
+++ lista de compras/script.js
@@ -0,0 +1,24 @@
+function adicionaritem() {
+    const input = document.getElementById('item');
+    const lista = document.getElementById('lista');
+
+    if (input.value.trim() === "") {
+        alert("escreva algo antes de adicionar");
+        return;
+    }
+    const novoItem = document.createElement('li');
+
+   novoItem.innerHTML = `
+        <span>${input.value}</span>
+        <button class="btn-apagar" onclick="removerItem(this)">Apagar</button>
+    `;
+
+      lista.appendChild(novoItem);
+
+    input.value = "";
+    input.focus();
+}
+function removerItem(botao) {
+    botao.parentElement.remove();
+
+}
